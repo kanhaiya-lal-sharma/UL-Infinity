60262936aa6f7a8c05df2460e0b549a3080cfd6d
Kanhaiya Sharma <kanhaiya.sharma@universityliving.com>
2026-03-30 15:14:25 +0530

    updated

--- tests/VAS Detail/guarantor.spec.js
+++ tests/VAS Detail/guarantor.spec.js
@@ -1,47 +1,4 @@
 
-// const{test,expect}=require("@playwright/test");
-
-// test("Guarantor form",async({page})=>{
-
-//      await page.goto("/");
-
-//   await page.getByRole("button", { name: "Housing" }).click();
-
-//   await page
-//     .getByRole("navigation")
-//     .getByRole("link", { name: "Guarantor", exact: true })
-//     .click();
-
-//   await expect(page).toHaveURL(/guarantor/);
-
-//   const findGuarantorBtn =  page.getByRole("button",{name:'Find A Guarantor'}).first();
-
-//   await findGuarantorBtn.click();
-
-// const guarantorModal = page .getByText("Leave us your contact details and one of our agents will get in touch with you soon.")
-//   .locator("..");
-
-// await guarantorModal.waitFor({ state: "visible" });
-
-// await guarantorModal.locator('input[name="firstName"]').fill("kanhaiya");
-
-// await guarantorModal.locator('input[name="lastName"]').fill("lal");
-
-// await guarantorModal.locator('input[name="email"]').fill("kanhaiya@yopmail.com");
-
-// const phoneInput = guarantorModal.locator('.react-tel-input input');
-
-// await phoneInput.click();
-// await page.keyboard.press('Control+A'); // select all (+91)
-// await page.keyboard.press('Backspace'); // delete
-// await phoneInput.type('8851658991');
-
-
-// const submitBtn = guarantorModal.getByRole("button",{name:'Submit'});
-
-// await submitBtn.click();
-
-// })
 
 
 const { test, expect } = require("@playwright/test");
